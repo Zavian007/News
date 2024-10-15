a98d8d4c8bc44da29074ad77c763a26a1a70d63b
smangaliso Nkanyezi Ntshangase <150956284+Smanga-28@users.noreply.github.com>
2024-10-15 15:12:33 +0200

    Update App.js

--- News/newss/src/App.js
+++ News/newss/src/App.js
@@ -1,19 +1,9 @@
-
 import './App.css';
 import Newsapp from './Components/Newsapp';
 
-
 function App() {
-<<<<<<< HEAD
-  return ( <Newsapp/>
-         
-=======
-  return ( 
-    
-  <Newsapp/>
- 
- 
->>>>>>> 7db0cd4 (Your commit message here)
+  return (
+    <Newsapp />
   );
 }
 
